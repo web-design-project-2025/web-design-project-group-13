2f596756061a23375a90cf5a36cbbb5023a4bf84
Edvinnn22 <edvin.hultqvist@icloud.com>
2025-05-16 12:16:34 +0200

    logo switchout <=768px

--- javascript/hamburger.js
+++ javascript/hamburger.js
@@ -12,26 +12,11 @@ document.addEventListener("DOMContentLoaded", function () {
       menu.classList.remove("open");
     }
   });
-
-  // This script handles the logo resizing based on screen width
-  // const logo = document.getElementById("logo");
-
-  // function updateLogo() {
-  //   if (window.innerWidth <= 768) {
-  //     logo.src = "/images/logoPhone.svg"; // Use SVG for smaller screens
-  //   } else {
-  //     logo.src = "/images/LettuceCookLogo.png"; // Use PNG for larger screens
-  //   }
-  // }
-
-  // // Initial check
-  // updateLogo();
-
-  // // Update logo on window resize
-  // window.addEventListener("resize", updateLogo);
 });
 
 // This script handles the logo click event to redirect to the index page
-document.getElementById("logo").addEventListener("click", function () {
-  window.location.href = "index.html";
+document.addEventListener("DOMContentLoaded", function () {
+  document.getElementById("logo").addEventListener("click", function () {
+    window.location.href = "index.html";
+  });
 });
